5f4976463c611523c19d7297a4f05f1faac44ac9
nafiz Alam <nafizalam.dev@gmail.com>
2026-05-12 21:56:51 +0600

    Query MongoDB to get user data

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-// simpleCrudUser
-// KSZmFLI9b6afJjR4
+const uri = `mongodb+srv://simpleCrudUser:KSZmFLI9b6afJjR4@cluster0.uwxm1bp.mongodb.net/?appName=Cluster0`;
 
-// mongodb+srv://simpleCrudUser:KSZmFLI9b6afJjR4@cluster0.uwxm1bp.mongodb.net/?appName=Cluster0
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const db = client.db('simpleCrud');
+        const userCollection = db.collection('users');
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        await client.db('admin').command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.use(cors());
 app.use(express.json());
